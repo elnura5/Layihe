3a9efd261a1ba5860815f07be52a065522f0e36d
elnura57 <elnuraash-bp219@code.edu.az>
2025-06-07 23:52:22 +0400

    folderleri yaratdim

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import AllCartoons from './Pages/AllCartoons';
+import Categories from './Pages/Categories';
+import VideoPlayer from './Pages/VideoPlayer';
+import About from './Pages/About';
+import Favorites from './Pages/Favorites';
+import MainLayout from './Layout/MainLayout';
+
+function App() {
+ 
+
+  return (
+    <>
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+        <Route  index element={<Home />} />
+        <Route path="/cartoons" element={<AllCartoons />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/video/:id" element={<VideoPlayer />} />
+        <Route path="/about" element={<About />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
+      
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
